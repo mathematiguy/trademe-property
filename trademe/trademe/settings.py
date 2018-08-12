84f486e7ad674a8d4b64ce47f4c3f0f7b9c85482
Caleb <caleb@dragonfly.co.nz>
2018-08-13 08:42:48 +1200

    Scaled down spider

--- trademe/trademe/settings.py
+++ trademe/trademe/settings.py
@@ -22,12 +22,13 @@ USER_AGENT = 'Caleb Moses - calebjdmoses@gmail.com - https://github.com/mathemat
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
